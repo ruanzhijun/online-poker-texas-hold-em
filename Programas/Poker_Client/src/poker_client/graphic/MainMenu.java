5055a32e9befebda0acc2c68dd7bbd80c47f774d
Mario Codes <mariocodes1993@gmail.com>
2017-06-03 13:19:48 +0200

    Added buttons which open my web page and another one to the rules of poker texas.

--- Programas/Poker_Client/src/poker_client/graphic/MainMenu.java
+++ Programas/Poker_Client/src/poker_client/graphic/MainMenu.java
@@ -1,12 +1,15 @@
 package poker_client.graphic;
 
 import entities.Player;
+import java.awt.Desktop;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -76,12 +79,11 @@ public class MainMenu extends javax.swing.JFrame {
         jButtonExit = new javax.swing.JButton();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenuAplication = new javax.swing.JMenu();
-        jMenuItemSignUp = new javax.swing.JMenuItem();
-        jMenuItemLostPassword = new javax.swing.JMenuItem();
         jMenuItemOpenWeb = new javax.swing.JMenuItem();
+        jMenuItemSignUp = new javax.swing.JMenuItem();
         jMenuConfiguration = new javax.swing.JMenu();
         jMenuHelp = new javax.swing.JMenu();
-        jMenuItemLearnPlay = new javax.swing.JMenuItem();
+        jMenuItemRules = new javax.swing.JMenuItem();
         jMenuItemDocumentation = new javax.swing.JMenuItem();
         jMenuItemAbout = new javax.swing.JMenuItem();
 
@@ -159,12 +161,6 @@ public class MainMenu extends javax.swing.JFrame {
 
         jMenuAplication.setText("Aplication");
 
-        jMenuItemSignUp.setText("Sign Up");
-        jMenuAplication.add(jMenuItemSignUp);
-
-        jMenuItemLostPassword.setText("Lost Password");
-        jMenuAplication.add(jMenuItemLostPassword);
-
         jMenuItemOpenWeb.setText("Open Web");
         jMenuItemOpenWeb.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -173,20 +169,36 @@ public class MainMenu extends javax.swing.JFrame {
         });
         jMenuAplication.add(jMenuItemOpenWeb);
 
+        jMenuItemSignUp.setText("Sign Up");
+        jMenuItemSignUp.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItemSignUpActionPerformed(evt);
+            }
+        });
+        jMenuAplication.add(jMenuItemSignUp);
+
         jMenuBar1.add(jMenuAplication);
 
         jMenuConfiguration.setText("Config");
+        jMenuConfiguration.setEnabled(false);
         jMenuBar1.add(jMenuConfiguration);
 
         jMenuHelp.setText("Help");
 
-        jMenuItemLearnPlay.setText("Learn to Play");
-        jMenuHelp.add(jMenuItemLearnPlay);
+        jMenuItemRules.setText("Rules");
+        jMenuItemRules.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItemRulesActionPerformed(evt);
+            }
+        });
+        jMenuHelp.add(jMenuItemRules);
 
         jMenuItemDocumentation.setText("Documentation");
+        jMenuItemDocumentation.setEnabled(false);
         jMenuHelp.add(jMenuItemDocumentation);
 
         jMenuItemAbout.setText("About...");
+        jMenuItemAbout.setEnabled(false);
         jMenuHelp.add(jMenuItemAbout);
 
         jMenuBar1.add(jMenuHelp);
@@ -249,7 +261,9 @@ public class MainMenu extends javax.swing.JFrame {
     }//GEN-LAST:event_jButtonNewGameActionPerformed
 
     private void jMenuItemOpenWebActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItemOpenWebActionPerformed
-        // TODO add your handling code here:
+        try {
+            Desktop.getDesktop().browse(new URI("http://mariocodes.com/proyectos/poker-java"));
+        } catch(IOException | URISyntaxException ex) { ex.printStackTrace(); }
     }//GEN-LAST:event_jMenuItemOpenWebActionPerformed
 
     private void jButtonExitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonExitActionPerformed
@@ -365,6 +379,18 @@ public class MainMenu extends javax.swing.JFrame {
             } else JOptionPane.showConfirmDialog(rootPane, "Wrong User and / or Password.", "Failed", JOptionPane.OK_CANCEL_OPTION);
         } 
     }//GEN-LAST:event_jButtonLogInActionPerformed
+
+    private void jMenuItemSignUpActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItemSignUpActionPerformed
+        try {
+            Desktop.getDesktop().browse(new URI("http://mariocodes.com/proyectos/poker-java/sign-up"));
+        } catch(IOException | URISyntaxException ex) { ex.printStackTrace(); }
+    }//GEN-LAST:event_jMenuItemSignUpActionPerformed
+
+    private void jMenuItemRulesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItemRulesActionPerformed
+        try {
+            Desktop.getDesktop().browse(new URI("https://en.wikipedia.org/wiki/Texas_hold_%27em#Rules"));
+        } catch(IOException | URISyntaxException ex) { ex.printStackTrace(); }
+    }//GEN-LAST:event_jMenuItemRulesActionPerformed
 
     // todo: delete me!. Testing.
     private static void round(Player o, Player a) {
@@ -490,9 +516,8 @@ public class MainMenu extends javax.swing.JFrame {
     private javax.swing.JMenu jMenuHelp;
     private javax.swing.JMenuItem jMenuItemAbout;
     private javax.swing.JMenuItem jMenuItemDocumentation;
-    private javax.swing.JMenuItem jMenuItemLearnPlay;
-    private javax.swing.JMenuItem jMenuItemLostPassword;
     private javax.swing.JMenuItem jMenuItemOpenWeb;
+    private javax.swing.JMenuItem jMenuItemRules;
     private javax.swing.JMenuItem jMenuItemSignUp;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
